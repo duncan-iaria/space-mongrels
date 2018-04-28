de59527669f11f5e1ae2d8d8fb94b81c9f924d17
Duncan Iaria <duncaniaria@gmail.com>
2018-04-28 10:41:46 -0400

    🐛 big change for GUI, renamed GUI to  GameGUI to avoid namespace conflicts with Editor.GUI

---
+++ SpaceMongrelsDeux/Assets/SM/Source/Core/Events/Editor/EventEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SNDL
+{
+    [CustomEditor(typeof(GameEvent))]
+    public class EventEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            GameEvent tempEvent = target as GameEvent;
+            if (GUILayout.Button("Raise Event"))
+            {
+                tempEvent.raise();
+            }
+        }
+    }
+}
---
+++ SpaceMongrelsDeux/Assets/SM/Source/Core/Events/GameEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SNDL
+{
+    [CreateAssetMenu]
+    public class GameEvent : ScriptableObject
+    {
+        /// <summary>
+        /// The list of listeners that this event will notify if it is raised.
+        /// </summary>
+        private readonly List<GameEventListener> eventListeners =
+            new List<GameEventListener>();
+
+        public void raise()
+        {
+            for (int i = eventListeners.Count - 1; i >= 0; i--)
+                eventListeners[i].onEventRaised();
+        }
+
+        public void registerListener(GameEventListener listener)
+        {
+            if (!eventListeners.Contains(listener))
+            {
+                eventListeners.Add(listener);
+            }
+        }
+
+        public void unregisterListener(GameEventListener listener)
+        {
+            if (eventListeners.Contains(listener))
+            {
+                eventListeners.Remove(listener);
+            }
+        }
+    }
+}
